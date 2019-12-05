4d14a3e1a496a5fcd3f4dcb19171d0fcd3aac650
jiah_kim <jia20152369@gmail.com>
2019-12-05 11:48:06 +0900

    ResourceBundle test

---
+++ src/ResourceBundleTest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ResourceBundleTest {
+	public static void main(String[] args) {
+		String resourceName="Hello";
+		ResourceBundle rb;
+		
+		rb=ResourceBundle.getBundle(resourceName);
+		
+	    rb = ResourceBundle.getBundle(resourceName, Locale.KOREA);
+	    System.out.println("한국 : " + rb.getString("hi"));
+	    System.out.println("한국 : " + rb.getString("bye"));
+	    
+	    rb = ResourceBundle.getBundle(resourceName, Locale.US); 
+	    System.out.println("미국 : " + rb.getString("hi"));
+	    System.out.println("미국 : " + rb.getString("bye"));
+	}
+}
